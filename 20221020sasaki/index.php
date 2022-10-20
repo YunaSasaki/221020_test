799e89262b8884eb1583c20058b38b67c9efb8ef
YunaSasaki <yuna_miwa098@outlook.jp>
2022-10-20 20:02:26 +0900

    first-commit

---
+++ 20221020sasaki/index.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="CSS/style.css">
+<table>
+  <?php
+  for ($a = 1; $a <= 9; $a++) {
+    echo '<tr>';
+    for ($b = 1; $b <= 9; $b++) {
+      echo '<td>' . $a * $b . '</td>';
+    }
+    echo '</tr>';
+  }
+  ?>
+</table>
